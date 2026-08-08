e0d80fed23bb4b238b763dc09bcc2bd097f704ca
anaconduck <wahyukusuma29@gmail.com>
2026-08-08 09:58:00 +0700

    Add landing page, about page, and navigation bar

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ scaler=joblib.load('scaler.save')
 
 @app.route("/")
 def home():
+    return render_template("landing.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/form")
+def form():
     return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
